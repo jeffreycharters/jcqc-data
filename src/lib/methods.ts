3115d84eb717e686172af2ad6786303ba500078d
Jeffrey Charters <jeff.charters@gmail.com>
2023-01-07 09:40:14 -0500

    refactor element getting method

---
+++ src/lib/methods.ts
@@ -0,0 +1,21 @@
+import { pb } from './pocketbase';
+import type { ElementsResponse, MethodElementsResponse } from './pocketbase-types';
+
+
+export const getElementsByMethod = async (methodId: string) => {
+    const elements: ElementsResponse[] = await pb.collection('elements').getFullList(200, { sort: 'mass', filter: 'retired = false' });
+
+    // create a list of element IDs used by the method
+    const allElementsList: MethodElementsResponse[] = await pb.collection('methodElements').getFullList(200, { filter: `method = "${methodId}"` });
+    const elementIds = allElementsList.map(e => e.element)
+
+    // Check and see if each element is in that list or not.
+    const usedElements = elements.filter(e => elementIds.includes(e.id));
+    const unusedElements = elements.filter(e => !elementIds.includes(e.id));
+
+    return {
+        usedElements,
+        unusedElements,
+        allElementsList
+    }
+}
--- src/routes/edit/methods/[id]/+page.ts
+++ src/routes/edit/methods/[id]/+page.ts
@@ -1,5 +1,6 @@
+import { getElementsByMethod } from '$lib/methods';
 import { pb } from '$lib/pocketbase';
-import type { ElementsResponse, MethodElementsResponse, MethodsResponse } from '$lib/pocketbase-types';
+import type { MethodsResponse } from '$lib/pocketbase-types';
 import { redirect } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
@@ -16,21 +17,14 @@ export const load = (async ({ params }) => {
         throw redirect(302, '/edit/methods');
     }
 
-    const elements: ElementsResponse[] = await pb.collection('elements').getFullList(200, { sort: 'mass', filter: 'retired = false' });
-
-    // create a list of element IDs used by the method
-    const usedElementList: MethodElementsResponse[] = await pb.collection('methodElements').getFullList(200, { filter: `method = "${method.id}"` });
-    const elementIds = usedElementList.map(e => e.element)
-
-    // Check and see if each element is in that list or not.
-    const usedElements = elements.filter(e => elementIds.includes(e.id));
-    const unusedElements = elements.filter(e => !elementIds.includes(e.id));
+    const { usedElements, unusedElements, allElementsList } = await getElementsByMethod(method.id);
 
 
     return {
+        title: `Editing ${method.name}`,
         method,
         usedElements,
         unusedElements,
-        methodElements: usedElementList
+        methodElements: allElementsList
     };
 }) satisfies PageLoad;
