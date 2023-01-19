8205a8923b704b5492380db8e99e8c2a7594120b
Jeffrey Charters <jeff.charters@gmail.com>
2023-01-19 15:26:52 -0500

    start on the report page

--- src/routes/report/+page.svelte
+++ src/routes/report/+page.svelte
@@ -1,8 +1,19 @@
 <script lang="ts">
 	import { methodParams, reportData } from '$lib/stores';
 
-	console.log($methodParams);
-	console.log($reportData);
+	$: data = Array.from($reportData);
 </script>
 
-woo
+{#if $methodParams.elements.length != data[0][1]?.results?.size}
+	<div class="text-red-500 text-sm w-fit mx-auto">
+		Warning: expected {$methodParams.elements.length} element{$methodParams.elements.length === 1
+			? ''
+			: 's'}, found {data[0][1]?.results?.size}. Possible method mismatch.
+	</div>
+{/if}
+
+{#each data as [key, value], sample (sample)}
+	<div>
+		{key}
+	</div>
+{/each}
