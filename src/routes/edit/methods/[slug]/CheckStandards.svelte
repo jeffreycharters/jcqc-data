1f6110d66475f0d4f0bc0e918c6faf14b37ba528
Jeffrey Charters <jeff.charters@gmail.com>
2023-01-31 20:10:18 -0500

    accordion some of the edit panels

--- src/routes/edit/methods/[slug]/CheckStandards.svelte
+++ src/routes/edit/methods/[slug]/CheckStandards.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
 	import TextInput from '$lib/components/TextInput.svelte';
 	import { method } from '$lib/stores';
-	import BlankList from './BlankList.svelte';
+	import { fade } from 'svelte/transition';
 	import CheckStandardList from './CheckStandardList.svelte';
 
 	let newCalName = '';
 	let calMessage = '';
+	let open = false;
+	let addFormOpen = false;
+	let addFormDiv: HTMLDivElement;
+	let contentDiv: HTMLElement;
+
 	const createNewCheckStandard = async () => {
 		if (!newCalName) {
 			calMessage = 'Please add a blank name';
@@ -25,36 +30,91 @@
 </script>
 
 <div class="basic-border py-4 px-8 mt-4">
-	<h2>Check Standards</h2>
-
-	<div class="flex flex-col gap-4">
-		{#if $method.checkStandards && $method.checkStandards?.size > 0}
-			{#each Array.from($method.checkStandards).sort() as [_, checkStandard] (checkStandard.id)}
-				<CheckStandardList {checkStandard} />
-			{/each}
+	<div class="flex items-end gap-4">
+		<button class="flex gap-2 items-center" on:click={() => (open = !open)}>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-5 w-5 stroke-gray-400 transition-all {open ? 'rotate-90' : ''}"
+				viewBox="0 0 24 24"
+				stroke-width="2"
+				stroke="currentColor"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path d="M7 7l5 5l-5 5" />
+				<path d="M13 7l5 5l-5 5" />
+			</svg>
+			<h2 class="inline-flex gap-4">Check Standard Targets</h2>
+		</button>
+		{#if calMessage}
+			<div
+				class="text-sm text-amber-600 italic tracking-wide w-36 text-center"
+				in:fade={{ duration: 200 }}
+				out:fade={{ duration: 100 }}
+			>
+				{calMessage}
+			</div>
 		{/if}
+	</div>
 
-		<div class="basic-border p-4 w-fit">
-			<h3>Add New Check Standard</h3>
-			<form class="w-48">
-				<TextInput
-					label="Check Standard Name"
-					placeholder="e.g. Calibration Check"
-					name="cal-name"
-					bind:value={newCalName}
-				/>
+	<div
+		class="overflow-hidden transition-all"
+		style="max-height: {open ? `${contentDiv.scrollHeight}px` : '0'}"
+		bind:this={contentDiv}
+	>
+		<div class="flex flex-col gap-4 mt-4">
+			{#if $method.checkStandards && $method.checkStandards?.size > 0}
+				{#each Array.from($method.checkStandards).sort() as [_, checkStandard] (checkStandard.id)}
+					<CheckStandardList {checkStandard} />
+				{/each}
+			{/if}
 
-				<div class="flex flex-col gap-2 items-start">
-					<div class="text-sm text-red-500">{calMessage ?? ''}</div>
-					<input
-						type="submit"
-						value="+Add"
-						class="btn font-semibold w-full"
-						on:click|preventDefault={createNewCheckStandard}
-					/>
-					// TODO: Make this collapsible
+			<div class="basic-border p-4 w-fit">
+				<button class="flex items-center gap-2" on:click={() => (addFormOpen = !addFormOpen)}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-5 w-5 stroke-gray-400 transition-all {addFormOpen ? 'rotate-90' : ''}"
+						viewBox="0 0 24 24"
+						stroke-width="2"
+						stroke="currentColor"
+						fill="none"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+						<path d="M7 7l5 5l-5 5" />
+						<path d="M13 7l5 5l-5 5" />
+					</svg>
+					<h3>Add New Check Standard</h3>
+				</button>
+
+				<div
+					class="overflow-hidden transition-all"
+					style="max-height: {addFormOpen ? `${addFormDiv.scrollHeight}px` : '0'}"
+					bind:this={addFormDiv}
+				>
+					<form class="w-48">
+						<TextInput
+							label="Check Standard Name"
+							placeholder="e.g. Calibration Check"
+							name="cal-name"
+							bind:value={newCalName}
+						/>
+
+						<div class="flex flex-col gap-2 items-start">
+							<div class="text-sm text-red-500">{calMessage ?? ''}</div>
+							<input
+								type="submit"
+								value="+Add"
+								class="btn font-semibold w-full"
+								on:click|preventDefault={createNewCheckStandard}
+							/>
+						</div>
+					</form>
 				</div>
-			</form>
+			</div>
 		</div>
 	</div>
 </div>
