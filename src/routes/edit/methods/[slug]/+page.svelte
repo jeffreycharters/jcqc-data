c137938d821337fdbbf6000c0a23323e959e9974
Jeffrey Charters <jeff.charters@gmail.com>
2023-01-31 19:26:13 -0500

    extract method edit into component

--- src/routes/edit/methods/[slug]/+page.svelte
+++ src/routes/edit/methods/[slug]/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import NumberInput from '$lib/components/NumberInput.svelte';
-	import TextInput from '$lib/components/TextInput.svelte';
 	import { method } from '$lib/stores';
 	import type { PageData } from './$types';
 	import LOQs from './LOQs.svelte';
@@ -12,6 +10,7 @@
 	import type { Analyte } from '$lib/classes';
 	import CheckStandards from './CheckStandards.svelte';
 	import ReferenceMaterials from './ReferenceMaterials.svelte';
+	import EditMethod from './EditMethod.svelte';
 
 	export let data: PageData;
 	let { elementList } = data;
@@ -39,33 +38,6 @@
 		}
 	});
 
-	let formMessage = '';
-
-	const addFormMessage = (message: string, timeout: number = 3000) => {
-		formMessage = message;
-		setTimeout(() => (formMessage = ''), timeout);
-	};
-
-	const editMethod = async () => {
-		if (!$method.name || !$method.calibrationCount) {
-			addFormMessage('Missing something');
-			return;
-		}
-		try {
-			$method.updateProperties({
-				name: $method.name,
-				calibrationCount: $method.calibrationCount,
-				description: $method.description,
-				checkStandardTolerance: $method.checkStandardTolerance,
-				rpdLimit: $method.rpdLimit
-			});
-			addFormMessage('Saved');
-		} catch (err) {
-			const error = err as Error;
-			addFormMessage(error.message);
-		}
-	};
-
 	const addElement = async (elementId: string) => {
 		await $method.addElement(elementId);
 		$method = $method;
@@ -79,97 +51,46 @@
 
 <h1>{$method.title}</h1>
 
-<div>
-	<div class="basic-border mt-8 px-8 py-4 w-fit">
-		<div class="flex items-baseline gap-4">
-			<h2>Edit Method</h2>
-			<div>// Todo: Make this auto-save.</div>
-		</div>
-		<form on:submit|preventDefault={editMethod}>
-			<div class="grid grid-cols-2 gap-x-12">
-				<TextInput
-					name="method-name"
-					placeholder="e.g. TOXI-064 or Serum Iodine"
-					bind:value={$method.name}
-					label="Method Name"
-				/>
-				<TextInput
-					name="description"
-					placeholder="e.g. Metals in serum"
-					bind:value={$method.description}
-					label="Method Description"
-				/>
-				<NumberInput
-					name="cal-count"
-					label="Number of non-blank calibration standards"
-					bind:value={$method.calibrationCount}
-					placeholder="e.g. 6"
-				/>
-				<NumberInput
-					name="rpd-limit"
-					label="RPD Warning limit (%)"
-					bind:value={$method.rpdLimit}
-					placeholder="e.g. 15"
-				/>
-				<NumberInput
-					name="check-standard-limit"
-					label="Check Standard Tolerance (%)"
-					bind:value={$method.checkStandardTolerance}
-					placeholder="e.g. 15"
-				/>
-				<div class="self-end">
-					{#if formMessage}
-						<div class="text-sm lm-2" in:fade={{ duration: 200 }} out:fade={{ duration: 100 }}>
-							{formMessage}
-						</div>
-					{/if}
-					<input class="btn my-2" type="submit" value="Save Changes" />
-				</div>
-			</div>
-		</form>
-	</div>
-</div>
+<EditMethod />
 
-<div class="mt-8 w-full">
-	<div class="basic-border py-4 px-6">
-		<div class="flex justify-between items-baseline">
-			<h2 class="mb-4">
-				Elements used by this method
-				<span class="text-gray-400">[{usedElements?.length}]</span>
-			</h2>
-			<div class="text-gray-500">
-				//TODO: add save indicators to this box || Changes in this box are saved as soon as they are
-				made.
-			</div>
-		</div>
-
-		<div class="grid grid-cols-8 gap-4">
-			{#each usedElements.sort((a, b) => (a.mass < b.mass ? -1 : 1)) as element (element.id)}
-				<div
-					class="col-span-2"
-					in:receive|local={{ key: element.id }}
-					out:send|local={{ key: element.id }}
-					animate:flip={{ duration: 150 }}
-				>
-					<ActiveElement {element} on:removeElement={(event) => removeElement(event.detail)} />
-				</div>
-			{/each}
-
-			{#each unusedElements.sort((a, b) => (a.mass < b.mass ? -1 : 1)) as element (element.id)}
-				<div
-					in:receive|local={{ key: element.id }}
-					out:send|local={{ key: element.id }}
-					animate:flip={{ duration: 150 }}
-				>
-					<InactiveElement {element} on:addElement={(event) => addElement(event.detail)} />
-				</div>
-			{/each}
+<div class="basic-border py-4 px-6">
+	<div class="flex justify-between items-baseline">
+		<h2 class="mb-4">
+			Elements used by this method
+			<span class="text-gray-400">[{usedElements?.length}]</span>
+		</h2>
+		<div class="text-gray-500">
+			//TODO: add save indicators to this box || Changes in this box are saved as soon as they are
+			made.
 		</div>
 	</div>
 
-	<CheckStandards />
+	<div class="grid grid-cols-8 gap-4">
+		{#each usedElements.sort((a, b) => (a.mass < b.mass ? -1 : 1)) as element (element.id)}
+			<div
+				class="col-span-2"
+				in:receive|local={{ key: element.id }}
+				out:send|local={{ key: element.id }}
+				animate:flip={{ duration: 150 }}
+			>
+				<ActiveElement {element} on:removeElement={(event) => removeElement(event.detail)} />
+			</div>
+		{/each}
 
-	<LOQs />
-
-	<ReferenceMaterials />
+		{#each unusedElements.sort((a, b) => (a.mass < b.mass ? -1 : 1)) as element (element.id)}
+			<div
+				in:receive|local={{ key: element.id }}
+				out:send|local={{ key: element.id }}
+				animate:flip={{ duration: 150 }}
+			>
+				<InactiveElement {element} on:addElement={(event) => addElement(event.detail)} />
+			</div>
+		{/each}
+	</div>
 </div>
+
+<CheckStandards />
+
+<LOQs />
+
+<ReferenceMaterials />
---
+++ src/routes/edit/methods/[slug]/EditMethod.svelte
@@ -0,0 +1,122 @@
+<script lang="ts">
+	import NumberInput from '$lib/components/NumberInput.svelte';
+	import TextInput from '$lib/components/TextInput.svelte';
+	import { method } from '$lib/stores';
+	import { fade } from 'svelte/transition';
+
+	let formMessage = '';
+	let timer: NodeJS.Timer;
+
+	let open = false;
+	let contentDiv: HTMLElement;
+
+	const addFormMessage = (message: string, timeout: number = 3000) => {
+		if (timer) clearTimeout(timer);
+		formMessage = message;
+		timer = setTimeout(() => (formMessage = ''), timeout);
+	};
+
+	const editMethod = async () => {
+		if (!$method.name || !$method.calibrationCount) {
+			addFormMessage('Missing something');
+			return;
+		}
+		try {
+			$method.updateProperties({
+				name: $method.name,
+				calibrationCount: $method.calibrationCount,
+				description: $method.description,
+				checkStandardTolerance: $method.checkStandardTolerance,
+				rpdLimit: $method.rpdLimit
+			});
+			addFormMessage('Saved!');
+		} catch (err) {
+			const error = err as Error;
+			addFormMessage(error.message);
+		}
+	};
+
+	function debounce(callback: () => void, timeout = 1000) {
+		return (...args: any) => {
+			addFormMessage('Changes pending...');
+			clearTimeout(timer);
+			timer = setTimeout(() => {
+				callback.apply(debounce, args);
+			}, timeout);
+		};
+	}
+
+	const processUpdate = () => debounce(editMethod);
+</script>
+
+<div class="basic-border my-4 px-8 py-4 w-full">
+	<div class="flex items-end gap-4">
+		<button class="flex gap-2 items-center" on:click={() => (open = !open)}>
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-5 w-5 stroke-gray-400 transition-all {open ? 'rotate-90' : ''}"
+				viewBox="0 0 24 24"
+				stroke-width="2"
+				stroke="currentColor"
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+			>
+				<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+				<path d="M7 7l5 5l-5 5" />
+				<path d="M13 7l5 5l-5 5" />
+			</svg>
+			<h2>Edit Method</h2>
+		</button>
+		{#if formMessage}
+			<div
+				class="text-sm text-amber-600 italic tracking-wide w-36 text-center"
+				in:fade={{ duration: 200 }}
+				out:fade={{ duration: 100 }}
+			>
+				{formMessage}
+			</div>
+		{/if}
+	</div>
+
+	<div
+		class="overflow-hidden transition-all"
+		style="max-height: {open ? `${contentDiv.scrollHeight}px` : '0'} "
+		bind:this={contentDiv}
+	>
+		<form on:input={processUpdate()} on:submit|preventDefault>
+			<div class="grid grid-cols-2">
+				<TextInput
+					name="method-name"
+					placeholder="e.g. TOXI-064 or Serum Iodine"
+					bind:value={$method.name}
+					label="Method Name"
+				/>
+				<TextInput
+					name="description"
+					placeholder="e.g. Metals in serum"
+					bind:value={$method.description}
+					label="Method Description"
+				/>
+				<NumberInput
+					name="cal-count"
+					label="Number of non-blank calibration standards"
+					bind:value={$method.calibrationCount}
+					placeholder="e.g. 6"
+				/>
+				<NumberInput
+					name="rpd-limit"
+					label="RPD Warning limit (%)"
+					bind:value={$method.rpdLimit}
+					placeholder="e.g. 15"
+				/>
+				<NumberInput
+					name="check-standard-limit"
+					label="Check Standard Tolerance (%)"
+					bind:value={$method.checkStandardTolerance}
+					placeholder="e.g. 15"
+				/>
+			</div>
+		</form>
+	</div>
+</div>
