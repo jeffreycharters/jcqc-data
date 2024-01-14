beb56602f19b00599c32f341d6916f1af8fbc285
Jeffrey Charters <jeff.charters@gmail.com>
2024-01-14 13:32:37 -0500

    add meltui+pp

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,22 @@
+import { preprocessMeltUI, sequence } from "@melt-ui/pp"
 import preprocess from "svelte-preprocess"
 import adapter from "@sveltejs/adapter-static"
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte"
-
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import('@sveltejs/kit').Config}*/
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
 	// for more information about preprocessors
-	preprocess: [
+	preprocess: sequence([
 		vitePreprocess(),
 		preprocess({
 			postcss: true
-		})
-	],
-
+		}),
+		preprocessMeltUI()
+	]),
 	kit: {
 		adapter: adapter({
 			fallback: "index.html"
 		})
 	}
 }
-
 export default config
