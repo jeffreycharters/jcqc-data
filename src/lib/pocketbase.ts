2e143397dd26a46e5c7d22c6dd7322fa77700a5f
Jeffrey Charters <jeff.charters@gmail.com>
2024-01-14 11:36:21 -0500

    basic prod port setting

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,17 +1,20 @@
-import PocketBase from 'pocketbase';
-import { writable } from 'svelte/store';
+import { dev } from "$app/environment"
+import PocketBase from "pocketbase"
+import { writable } from "svelte/store"
 
-export const pb = new PocketBase('http://127.0.0.1:8090');
-pb.autoCancellation(false);
+const HOST = dev ? "http://127.0.0.1:8090" : "http://0.0.0.0:8090"
 
-export const currentUser = writable(pb.authStore.model);
+export const pb = new PocketBase(HOST)
+pb.autoCancellation(false)
+
+export const currentUser = writable(pb.authStore.model)
 
 export const logout = () => {
-	pb.authStore.clear();
-	currentUser.set(null);
-};
+	pb.authStore.clear()
+	currentUser.set(null)
+}
 
 pb.authStore.onChange((/* auth */) => {
 	// console.log('authStore changed', auth);
-	currentUser.set(pb.authStore.model);
-});
+	currentUser.set(pb.authStore.model)
+})
